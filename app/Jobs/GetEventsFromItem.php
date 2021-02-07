ef5632ac6f6a2d55597e9893cc29841cc212d6e9
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-02-07 14:25:21 +0900

    分析メソッドを3つ追加

--- app/Jobs/GetEventsFromItem.php
+++ app/Jobs/GetEventsFromItem.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Carbon\Carbon;
+use Carbon\Exceptions\RuntimeException;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -37,6 +38,7 @@ class GetEventsFromItem implements ShouldQueue
      */
     public function handle()
     {
+//        Carbon::setTestNow(Carbon::today()->subMonth(4));
         $events_arr = [];
         for ($i = 0; $i <= 20; $i++) {
             $events_arr = array_merge($events_arr, $this->getEventsFromPage($i));
@@ -46,6 +48,10 @@ class GetEventsFromItem implements ShouldQueue
 
         echo(json_encode($all_events, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
         echo(json_encode($events_dates, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        echo('running:' . $this->getRunningDays($events_dates));
+        echo("\n" . 'latest:' . $this->getLatestDate($events_dates));
+        echo(Carbon::today()->subMonth(4));
+        echo("\n" . 'count:' . $this->countDays($events_dates, Carbon::today()->subMonth(4)));
     }
 
     private function getEventsFromPage(int $page)
@@ -70,9 +76,61 @@ class GetEventsFromItem implements ShouldQueue
     {
         return array_map(function ($event) {
             $datetime_format = function (string $datetime) {
-                return Carbon::parse($datetime)->subHour(self::DAY_START_HOUR)->toDateString();
+                return Carbon::parse($datetime)->subHour(self::DAY_START_HOUR);
             };
             return $datetime_format($event['event_date']);
         }, $events);
+    }
+
+    public function countDays(array $dates, Carbon $start_date): int
+    {
+        $tmp_date = Carbon::today();
+        $count = 0;
+
+        if ($start_date->gt($tmp_date)) {
+            throw new \RuntimeException("集計期間が無効です。");
+        }
+
+        while ($tmp_date->gt($start_date) || $tmp_date->isSameDay($start_date)) {
+            if ($this->existsSameDayInArray($tmp_date, $dates)) {
+                $count++;
+            }
+            $tmp_date->subDay();
+        }
+
+        return $count;
+    }
+
+    public function getRunningDays(array $dates): int
+    {
+        $tmp_date = Carbon::today();
+        $running_days = 0;
+
+        while ($this->existsSameDayInArray($tmp_date, $dates)) {
+            $running_days++;
+            $tmp_date->subDay();
+        }
+        return $running_days;
+    }
+
+    public function getLatestDate(array $dates): Carbon
+    {
+        $latest_date = Carbon::today();
+
+        while (!$this->existsSameDayInArray($latest_date, $dates)) {
+            $latest_date->subDay();
+        }
+        return $latest_date;
+    }
+
+    public function existsSameDayInArray(Carbon $needle, array $haystack): bool
+    {
+        foreach ($haystack as $hay) {
+            if ($hay->isSameday($needle)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
