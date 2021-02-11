f37ac7940841c7946d393a38555b4783f4fa5cf0
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-02-12 07:19:24 +0900

    TODOISTのCrient情報を追加

--- config/todoapp.php
+++ config/todoapp.php
@@ -4,6 +4,8 @@ return [
 
     'todoist' => [
         'api_key' => env('DUMMY_TODOIST_API_KEY'),
+        'client_id' => env('TODOIST_CLIENT_ID'),
+        'client_secret' => env('TODOIST_CLIENT_SECRET'),
     ],
 
 ];
