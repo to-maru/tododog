a4037380536b62f931744474464d2c867f0a874e
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-02-17 08:44:47 +0900

    add route info

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/settings', function () {
+        return view('settings'); //todo: 後で作る
+    });
+});
