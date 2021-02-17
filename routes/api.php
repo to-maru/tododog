a7f7c52c2b495ed2ede27dc478a2181029fdd6c2
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-02-17 10:09:26 +0900

    route情報だけ追加

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/api/auth/todoist', function (Request $request) {
+    return $request->user(); //todo: controllerを設定する
+});
