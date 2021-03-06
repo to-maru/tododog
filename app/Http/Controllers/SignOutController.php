ae3c32b4015e38b5c19c3442808e164c2401f918
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-03-07 02:38:44 +0900

    リネーム対応完了

--- app/Http/Controllers/SignOutController.php
+++ app/Http/Controllers/SignOutController.php
@@ -2,15 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Analyser;
-use App\Models\TodoApplication;
-use App\Models\User;
-use http\Exception\InvalidArgumentException;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Str;
 
 class SignOutController extends Controller
 {
