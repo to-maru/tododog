9858da006879322492f7baa577a21ecf5c6b0762
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-03-14 22:23:55 +0900

    新todoメソッドを追加・動作確認

--- app/Services/Synchronizer.php
+++ app/Services/Synchronizer.php
@@ -20,11 +20,11 @@ class Synchronizer
 
     public function syncronizeTodo($todo_application)
     {
-        $todos = $this->getAllTodoNames($this->api_client);
-        foreach ($todos as $key => $value) {
+        $todos = $this->getAllTodos($this->api_client);
+        foreach ($todos as $todo) {
             Todo::updateOrCreate(
-                ['todo_application_id' => $todo_application->id, 'local_id' => $key],
-                ['name' => $value]
+                ['todo_application_id' => $todo_application->id, 'local_id' => $todo['id']],
+                ['name' => $todo['content'], 'origin_created_at' => $todo['date_added'], 'project_name' => $todo['project_id'], 'raw_data' => $todo]
             );
         }
     }
