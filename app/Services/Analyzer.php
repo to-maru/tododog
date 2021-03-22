ec3b5cb92c2f72e582801560120e6edc0c2e6c4d
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-03-22 23:43:56 +0900

    project_idによるフィルタリングを追加

--- app/Services/Analyzer.php
+++ app/Services/Analyzer.php
@@ -4,12 +4,20 @@
 namespace App\Services;
 
 
+use App\Models\Todo;
 use App\Models\User;
 
 class Analyzer
 {
     public function analyze(User $user)
     {
+        $routine_watcher_setting = $user->routine_watcher_setting;
+        $arr = [];
+        $project_id = $routine_watcher_setting->project_id;
+        if ($project_id !== NULL) {
+            $arr[] = ['project_name', '=', $project_id];
+        }
 
+        $todos = Todo::where($arr)->count();
     }
 }
