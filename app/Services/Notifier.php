d0a6ed3db2bc7e31b08e8c057492b797b6a54690
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-03-28 13:23:46 +0900

    TodoUpdateOrderの修正

--- app/Services/Notifier.php
+++ app/Services/Notifier.php
@@ -9,8 +9,21 @@ class Notifier
     public function notify($results)
     {
         info($results);
+        $this->getTodoUpdateOrders($results);
         $todo_update_order = new TodoUpdateOrder(688);
         var_dump($todo_update_order);
         exit;
+    }
+
+    public function getTodoUpdateOrders($results)
+    {
+        foreach ($results as $todo_id => $result) {
+            $this->getTodoUpdateOrder($todo_id, $result);
+        }
+    }
+
+    public function getTodoUpdateOrder($todo_id, $result)
+    {
+
     }
 }
--- app/Services/TodoUpdateOrder.php
+++ app/Services/TodoUpdateOrder.php
@@ -4,18 +4,14 @@
 namespace App\Services;
 
 
-use App\Models\Todo;
-
 class TodoUpdateOrder
 {
-    public Todo $original;
-
     public function __construct(
-        public int $todo_id,
+        public int $local_id,
         public ?string $name = null,
         public ?array $tag_ids = null,
         public ?string $comment = null,
     ) {
-        $this->original = Todo::find($todo_id);
+
     }
 }
