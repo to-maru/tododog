ce760e0640f174bf16fcdcd014326ff6cb3cb28e
t-maruyama0815 <t.maruyama0815@gmail.com>
2021-05-08 16:49:49 +0900

    pcのみ対応のfixed side bar

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -21,10 +21,10 @@
     </x-slot>
     <div class="container-fluid h-100">
         <div class="row h-100">
-            <nav class="sidebar col-12 col-sm-2 sm:h-100 p-0" style="background-color: #CCBDB7;">
-                {{ $nav }}
+            <nav class="sidebar col-12 col-sm-4 col-md-3 col-lg-2 h-100 p-0 position-fixed top-0" style="background-color:#CCBDB7;">
+                    {{ $nav }}
             </nav>
-            <main class="col-10">
+            <main class="col-12 offset-sm-4 offset-md-3 offset-lg-2 col-sm-8 col-md-9 col-lg-10">
                 {{ $slot }}
             </main>
         </div>
